c4f12d75709aeeb04ee9f6e47cbbecc77e019d1f
Brayan <ocamposource@gmail.com>
2026-04-07 19:11:52 -0500

    feat: update main routes configuration

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,18 @@
 import { Application } from "express";
-
-import expenseTemplatesRouter from "./routes/expense-templates";
-import expenseRecordsRouter from "./routes/expense-records";
-import expenseCategoriesRouter from "./routes/expense-categories";
-import incomeTemplatesRouter from "./routes/income-templates";
-import incomeRecordsRouter from "./routes/income-records";
-import incomeCategoriesRouter from "./routes/income-categories";
-import usersRouter from "./routes/users";
-
-// ─── Routes /api ───────────────────────────────────────────────────────────────
+import expenseTemplatesRouter from "@routes/expense-templates";
+import expenseRecordsRouter from "@routes/expense-records";
+import expenseCategoriesRouter from "@routes/expense-categories";
+import incomeTemplatesRouter from "@routes/income-templates";
+import incomeRecordsRouter from "@routes/income-records";
+import incomeCategoriesRouter from "@routes/income-categories";
+import usersRouter from "@routes/users";
 
 export function registerRouter(app: Application): void {
-  app.use("/api/expense_templates", expenseTemplatesRouter);
-  app.use("/api/expense_categories", expenseCategoriesRouter);
-  app.use("/api/expense_records", expenseRecordsRouter);
-  app.use("/api/income_templates", incomeTemplatesRouter);
-  app.use("/api/income_categories", incomeCategoriesRouter);
-  app.use("/api/income_records", incomeRecordsRouter);
-  app.use("/api/users", usersRouter);
+  app.use("/v1/expense-templates", expenseTemplatesRouter);
+  app.use("/v1/expense-categories", expenseCategoriesRouter);
+  app.use("/v1/expense-records", expenseRecordsRouter);
+  app.use("/v1/income-templates", incomeTemplatesRouter);
+  app.use("/v1/income-categories", incomeCategoriesRouter);
+  app.use("/v1/income-records", incomeRecordsRouter);
+  app.use("/v1/users", usersRouter);
 }
