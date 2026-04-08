1bafa02ac309f6cf07c5d52cf3d3749b8953461c
JCiroLo <juanciro35@gmail.com>
2026-04-08 15:07:44 -0500

    fix: Update Supabase environment variable names in .env.example and supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,10 +4,10 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error("The SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY environment variables are missing from the .env file.");
+  throw new Error("The SUPABASE_URL or SUPABASE_KEY environment variables are missing from the .env file.");
 }
 
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
